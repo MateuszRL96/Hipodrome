a8a4ad8bc84b2f4da8e0f45d3773cfe85846da03
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-01-09 15:30:29 +0100

    sdasd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sdadad");
+    }
+}
+        //SnakeGame snakeGame = new SnakeGame("SnakeGame");
+
+        /*
+         SwingUtilities.invokeLater(() -> {
+            SnakeGame snakeGame = new SnakeGame();
+            snakeGame.setVisible(true);
+        });
+
+
