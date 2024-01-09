a83f06edeb8bf94a9b2c4d269e1ed4de3b073170
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-01-09 18:51:11 +0100

    6

--- src/main/java/org/example/Hippodrome.java
+++ src/main/java/org/example/Hippodrome.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.List;
 
 public class Hippodrome {
+    static Hippodrome game;
     private List<Horse> horses;
     public List<Horse> getHorses(){
         return horses;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,20 @@
 package org.example;
 
 import javax.swing.*;
+import java.util.ArrayList;
+
+import static org.example.Hippodrome.game;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("sdadad");
+        game = new Hippodrome(new ArrayList<>());
+        game.getHorses().add(new Horse("Kon1", 3,0));
+        game.getHorses().add(new Horse("Kon2", 3,0));
+        game.getHorses().add(new Horse("Kon3", 3,0));
+
     }
 }
-        //SnakeGame snakeGame = new SnakeGame("SnakeGame");
 
-        /*
-         SwingUtilities.invokeLater(() -> {
-            SnakeGame snakeGame = new SnakeGame();
-            snakeGame.setVisible(true);
-        });*/
 
 
